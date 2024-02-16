dca5f35eac9a1e2e65eecd7a46e26078eaa9030d
esmax20 <m.esbiza97@gmail.com>
2024-02-16 10:29:07 -0300

    Exercises 3.1 - 3.8

---
+++ index.js
@@ -0,0 +1,110 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+var morgan = require('morgan')
+morgan.token('cont', function (req) {
+    return (`{"name":"${req.body.name}", "number":"${req.body.number}"}`)
+  })
+
+app.use(express.json())
+
+
+app.use(morgan(':method :url :response-time :cont'))
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (request, response) => {
+    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${Date().toString()}</p>`)
+})
+  
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = persons.length > 0
+      ? Math.random() * 1000
+      : 0
+    return maxId + 1
+}
+  
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+  
+    if (!body.name) {
+      return response.status(400).json({ 
+        error: 'name missing' 
+      })
+    }
+    else {
+        if (persons.map(person => person.name).includes(body.name)) {
+            return response.status(400).json({ 
+                error: `name '${body.name}' already registered` 
+              })
+        }
+    }
+
+    if (!body.number) {
+        return response.status(400).json({ 
+          error: 'number missing' 
+        })
+    }
+    
+    
+  
+    const person = {
+      name: body.name,
+      number: body.number,
+      id: generateId(),
+    }
+  
+    persons = persons.concat(person)  
+    response.json(person)
+})
+  
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
