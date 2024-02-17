de20df2973520cc54cd4c76893be4595af456fc5
esmax20 <m.esbiza97@gmail.com>
2024-02-17 01:04:32 -0300

    uibuild

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const Person = require('./models/person')
 
 app.use(express.static('dist'))
 
-let persons = [
+/* let persons = [
     { 
       "id": 1,
       "name": "Arto Hellas", 
@@ -24,7 +26,7 @@ let persons = [
       "name": "Mary Poppendieck", 
       "number": "39-23-6423122"
     }
-]
+] */
 
 var morgan = require('morgan')
 morgan.token('cont', function (req) {
@@ -54,18 +56,17 @@ app.get('/info', (request, response) => {
 })
   
 app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
     response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        response.json(person)
-      } else {
-        response.status(404).end()
-      }
+  Person.findById(request.params.id).then(person => {
+    response.json(person)
+  })
 })
+
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
@@ -74,51 +75,44 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-const generateId = () => {
-    const maxId = persons.length > 0
-      ? Math.random() * 1000
-      : 0
-    return maxId + 1
-}
-  
+
 app.post('/api/persons', (request, response) => {
     const body = request.body
   
-    if (!body.name) {
-      return response.status(400).json({ 
-        error: 'name missing' 
-      })
+    if (body.name === undefined) {
+      return response.status(400).json({ error: 'name missing' })
     }
-    else {
-        if (persons.map(person => person.name).includes(body.name)) {
+    /* else {
+        if ((app.get('/api/persons', (request, response) => {
+          Person.find({}).then(persons => {
+            response.json(persons)
+          })
+        })).map(person => person.name).includes(body.name)) {
             return response.status(400).json({ 
                 error: `name '${body.name}' already registered` 
               })
         }
-    }
+    } */
 
-    if (!body.number) {
-        return response.status(400).json({ 
-          error: 'number missing' 
-        })
+    if (body.number === undefined) {
+        return response.status(400).json({ error: 'number missing' })
     }
     
     
-  
-    const person = {
+    const person = new Person({
       name: body.name,
       number: body.number,
-      id: generateId(),
-    }
+    })
   
-    persons = persons.concat(person)  
-    response.json(person)
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
+    })
 })
   
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
